f69fef7f3f54cfdb7c5118d4014c5a1104303e81
Barbara <barbaramax21@gmail.com>
2026-05-28 23:11:14 -0300

    teste

--- backend/build.sh
+++ backend/build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-cd /opt/render/project/src/frontend
+pip install -r requirements.txt
+
+cd ../frontend
 npm install
 npm run build
 
-cd /opt/render/project/src/backend
-
-/opt/render/project/src/.venv/bin/pip install -r requirements.txt
-
-python manage.py migrate --noinput
+cd ../backend
 python manage.py collectstatic --noinput
