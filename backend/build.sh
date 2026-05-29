ace92b98fe97de324ae464fedb6f24b08d778c91
Barbara <barbaramax21@gmail.com>
2026-05-28 22:16:31 -0300

    sigo tentando

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 set -e
 
-cd ../frontend
+cd /opt/render/project/src/frontend
 npm install
 npm run build
 
-cd ../backend
+cd /opt/render/project/src/backend
 
 pip install -r requirements.txt
 
